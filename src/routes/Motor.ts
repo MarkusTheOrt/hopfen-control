9682b8eaa05988342c4a7f931db2346234c0e69c
Markus Ort <markus@ort.dev>
2022-01-15 13:12:47 +0100

    inverted motor thing

--- src/routes/Motor.ts
+++ src/routes/Motor.ts
@@ -9,36 +9,36 @@ Motor.post("/motor/toggle", (req, res) => {
   AppStorage.motor = !AppStorage.motor;
   rpio.write(motorPins[0], AppStorage.motor ? rpio.HIGH : rpio.LOW);
   rpio.write(motorPins[1], AppStorage.motor ? rpio.HIGH : rpio.LOW);
-  res.json({ success: true, newState: AppStorage.motor });
+  res.json({ success: true, newState: !AppStorage.motor });
 });
 
 Motor.post("/motor/off", (req, res) => {
-  AppStorage.motor = false;
-  rpio.write(motorPins[0], AppStorage.motor ? rpio.HIGH : rpio.LOW);
-  rpio.write(motorPins[1], AppStorage.motor ? rpio.HIGH : rpio.LOW);
-  res.json({ success: true, newState: AppStorage.motor });
-});
-
-Motor.post("/motor/on", (req, res) => {
   AppStorage.motor = true;
   rpio.write(motorPins[0], AppStorage.motor ? rpio.HIGH : rpio.LOW);
   rpio.write(motorPins[1], AppStorage.motor ? rpio.HIGH : rpio.LOW);
-  res.json({ success: true, newState: AppStorage.motor });
+  res.json({ success: true, newState: !AppStorage.motor });
+});
+
+Motor.post("/motor/on", (req, res) => {
+  AppStorage.motor = false;
+  rpio.write(motorPins[0], AppStorage.motor ? rpio.HIGH : rpio.LOW);
+  rpio.write(motorPins[1], AppStorage.motor ? rpio.HIGH : rpio.LOW);
+  res.json({ success: true, newState: !AppStorage.motor });
 });
 
 Motor.get("/motor/", (req, res) => {
-  res.json({ led: AppStorage.led, motor: AppStorage.motor });
+  res.json({ led: AppStorage.led, motor: !AppStorage.motor });
 });
 
 Motor.post("/motor/turn/:state", (req, res) => {
   if (req.params.state === "true") {
-    AppStorage.motor = true;
-  } else {
     AppStorage.motor = false;
+  } else {
+    AppStorage.motor = true;
   }
   rpio.write(motorPins[0], AppStorage.motor ? rpio.HIGH : rpio.LOW);
   rpio.write(motorPins[1], AppStorage.motor ? rpio.HIGH : rpio.LOW);
-  res.json({ success: true, newState: AppStorage.motor });
+  res.json({ success: true, newState: !AppStorage.motor });
 });
 
 export default Motor;
