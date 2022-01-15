ed0c3f200e7eeecbe792a0b9e0a508ae4849ebae
Markus Ort <markus@markusort.com>
2022-01-15 20:07:38 +0100

    added button press.

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,12 @@ server.use(LED);
 
   while (true) {
     const val = rpio.read(buttonPin);
-    rpio.write(motorPins[0], val);
-    rpio.write(motorPins[1], val);
-    AppStorage.motor = val ? true : false;
+    if (AppStorage.lastButtonState != (val ? true : false)) {
+      AppStorage.lastButtonState = val ? true : false;
+      rpio.write(motorPins[0], val);
+      rpio.write(motorPins[1], val);
+      AppStorage.motor = val ? true : false;
+    }
     await new Promise((resolve) => setTimeout(resolve, 50));
   }
 })().catch((e) => {
--- src/utilities/AppStorage.ts
+++ src/utilities/AppStorage.ts
@@ -1,4 +1,5 @@
 export default {
   motor: false,
   led: false,
+  lastButtonState: false,
 };
